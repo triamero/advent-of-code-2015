353228327d2a8f4ec0c1709f21757e987fd609a4
triamero <4444613+triamero@users.noreply.github.com>
2019-10-01 21:44:05 +0300

    *add day10 with tests

---
+++ csharp/AdventOfCode2015.Tests/Day10Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2015.Tests
+{
+    public class Day10Tests
+    {
+        [TestCase("1", ExpectedResult = 82350)]
+        [TestCase("11", ExpectedResult = 107312)]
+        [TestCase("21", ExpectedResult = 139984)]
+        [TestCase("1211", ExpectedResult = 182376)]
+        public int GetAnswerPart1_(string input)
+        {
+            var result = (int)new Day10().GetAnswerPart1(input);
+
+            return result;
+        }
+
+        [TestCase("1", ExpectedResult = 1166642)]
+        [TestCase("11", ExpectedResult = 1520986)]
+        [TestCase("21", ExpectedResult = 1982710)]
+        [TestCase("1211", ExpectedResult = 2584304)]
+        public int GetAnswerPart2_(string input)
+        {
+            var result = (int)new Day10().GetAnswerPart2(input);
+
+            return result;
+        }
+    }
+}
---
+++ csharp/AdventOfCode2015/Day10.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace AdventOfCode2015
+{
+    public class Day10 : IDay
+    {
+        /// <inheritdoc />
+        public string Puzzle
+        {
+            get { return Resources.Day10; }
+        }
+
+        /// <inheritdoc />
+        public object GetAnswerPart1(string input)
+        {
+            var line = input;
+
+            for (int i = 0; i < 40; i++)
+            {
+                line = IterateString(line);
+            }
+
+            return line.Length;
+        }
+
+        /// <inheritdoc />
+        public object GetAnswerPart2(string input)
+        {
+            var line = input;
+
+            for (int i = 0; i < 50; i++)
+            {
+                line = IterateString(line);
+            }
+
+            return line.Length;
+        }
+
+        private static string IterateString(string line)
+        {
+            var newResult = new StringBuilder();
+
+            int i = 1;
+
+            char symbol = line[0];
+            int count = 1;
+
+            while (i < line.Length)
+            {
+                if (line[i] == symbol)
+                {
+                    count++;
+                }
+                else
+                {
+                    newResult.Append(count).Append(symbol);
+                    symbol = line[i];
+                    count = 1;
+                }
+
+                i++;
+            }
+
+            newResult.Append(count).Append(symbol);
+
+            return newResult.ToString();
+        }
+    }
+}
